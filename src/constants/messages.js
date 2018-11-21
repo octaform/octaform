5b8f23fe58755942286bbea1f393447a8c1890c3
Christian Fortes <chrisgfortes@gmail.com>
2018-11-21 16:10:23 -0200

    Changed messages of errors

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -3,8 +3,8 @@ import Package from '../../package.json';
 export default {
   CORE: {
     msg: `The validation #{method} has not been defined a message, please check out ${Package.repository.url}#validator-method-octaformvalidatoradd`,
-    field: `Field #{selector} was not found, please check out ${Package.repository.url}#validation-schema`,
-    add: `Add method accept only a list of object: [{ name: String, message: String, fn: Function }], please check out ${Package.repository.url}#validator-method-octaformvalidatoradd`,
+    field: `Field #{selector} was not found, please check out ${Package.repository.url}#field-name-required`,
+    add: `Add method accept only a list of object, please check out ${Package.repository.url}#validator-method-octaformvalidatoradd`,
     undefined: `The validation #{method} is not defined, please check out ${Package.repository.url}#validator-method-octaformvalidatoradd`,
     entry: `The entrypoint #{rules} has an invalid format, please check out ${Package.repository.url}#validation-schema`,
   },
