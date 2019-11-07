6ebc35f2b93d6403af669353150918bf94c64588
Christian Fortes <christian.fortes@iwsbrazil.com>
2019-11-07 02:08:20 -0300

    Fixed issue with empty schema validation

--- webpack/config.dev.js
+++ webpack/config.dev.js
@@ -1,8 +1,10 @@
 const CircularDependencyPlugin = require('circular-dependency-plugin');
 const merge = require('webpack-merge');
+const npmConfig = require('./config.npm');
 const webpackBase = require('./config.base');
 const glob = require('glob');
 const fs = require('fs');
+const GenerateJsonPlugin = require('generate-json-webpack-plugin');
 
 const markdown = glob.sync('./changelogs/*.md');
 
@@ -20,5 +22,6 @@ module.exports = merge(webpackBase, {
       exclude: /node_modules/,
       failOnError: false,
     }),
+    new GenerateJsonPlugin('package.json', npmConfig.package),
   ],
 });
